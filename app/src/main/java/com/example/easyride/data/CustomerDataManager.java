066ae3a099abb8a380f7d2872c2b3a1854390cbe
jaysinh01 <jaysinh@ualberta.ca>
2020-03-03 15:46:03 -0700

    almost done

---
+++ app/src/main/java/com/example/easyride/data/CustomerDataManager.java
@@ -0,0 +1,28 @@
+package com.example.easyride.data;
+
+import com.example.easyride.data.model.EasyRideUser;
+import com.example.easyride.data.model.RideRequest;
+
+public class RiderDataManager  extends DataBaseManager{
+
+    Boolean SignupUser(EasyRideUser user){
+
+        return true;
+    }
+
+    Boolean AcceptRideRequest(RideRequest rideRequest){
+        return true;
+    }
+    void RideRequest(){
+
+    }
+    void Update(){
+
+    }
+    Boolean VerifyUser(){
+        return true;
+    }
+}
+
+
+
