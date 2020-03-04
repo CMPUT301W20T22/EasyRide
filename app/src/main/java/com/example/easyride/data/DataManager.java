5aeb587bf559ff7bf3d18434cae3e04f7bfc4adf
jaysinh01 <jaysinh@ualberta.ca>
2020-03-04 12:50:26 -0700

    initialized database

--- app/src/main/java/com/example/easyride/data/DataManager.java
+++ app/src/main/java/com/example/easyride/data/DataManager.java
@@ -1,10 +1,21 @@
 package com.example.easyride.data;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
 import com.example.easyride.data.model.Driver;
 import com.example.easyride.data.model.EasyRideUser;
 import com.example.easyride.data.model.Rider;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * This class will handle the communication with database
@@ -17,13 +28,40 @@ public class DataManager {
 
 
     public boolean isDriver(String userID){
-        return false;
+        //https://stackoverflow.com/a/53335711
+        DocumentReference docIdRef = driverRef.document(userID);
+        final boolean[] result = new boolean[1];
+        docIdRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(TAG, "Document exists!");
+                        result[0] = true;
+                    } else {
+                        Log.d(TAG, "Document does not exist!");
+                        result[0] = false;
+                    }
+                } else {
+                    Log.d(TAG, "Failed with: ", task.getException());
+                    result[0] = false;
+                }
+            }
+        });
+        return result[0];
     }
 
     public EasyRideUser getDriver(String userID){
         //make the user class
-        driverRef.
-        return null;
+        EasyRideUser driver;
+        if(!isDriver(userID)){
+            throw new IllegalArgumentException("Invalid username");
+        }else{
+            DocumentSnapshot driverSnapshot = driverRef.document(userID).get().getResult();
+            driver = driverSnapshot.toObject(EasyRideUser.class);
+        }
+        return driver;
     }
 
     public void insertDriver(Driver driver){
@@ -32,12 +70,40 @@ public class DataManager {
     }
 
     public boolean isRider(String userID){
-        return false;
+        DocumentReference docIdRef = riderRef.document(userID);
+        final boolean[] result = new boolean[1];
+        docIdRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(TAG, "Document exists!");
+                        result[0] = true;
+                    } else {
+                        Log.d(TAG, "Document does not exist!");
+                        result[0] = false;
+                    }
+                } else {
+                    Log.d(TAG, "Failed with: ", task.getException());
+                    result[0] = false;
+                }
+            }
+        });
+        return result[0];
+
     }
 
     public EasyRideUser getRider(String userID){
         //make the user class
-        return null;
+        EasyRideUser rider;
+        if(!isRider(userID)){
+            throw new IllegalArgumentException("Invalid username");
+        }else{
+            DocumentSnapshot riderSnapshot = riderRef.document(userID).get().getResult();
+            rider = riderSnapshot.toObject(EasyRideUser.class);
+        }
+        return rider;
     }
 
     public void insertRider(Rider driver){
--- app/src/test/java/com/example/easyride/LoginUnitTest.java
+++ app/src/test/java/com/example/easyride/LoginUnitTest.java
@@ -1,4 +1,17 @@
 package com.example.easyride;
 
+import com.example.easyride.data.DataBaseManager;
+import com.example.easyride.data.DataManager;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class LoginUnitTest {
+
+    @Test
+    public void testIsUser(){
+        String driver = "jaysinh";
+        DataManager database = new DataManager();
+        assertTrue(database.isDriver(driver));
+    }
 }
