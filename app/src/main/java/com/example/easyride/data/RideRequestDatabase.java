0fc5a4b9185b0cdac73d4fbe6aea4ff9de2e6271
jaysinh01 <jaysinh@ualberta.ca>
2020-03-07 12:02:56 -0700

    query

--- app/src/main/java/com/example/easyride/data/RideRequestDatabase.java
+++ app/src/main/java/com/example/easyride/data/RideRequestDatabase.java
@@ -1,10 +1,21 @@
 package com.example.easyride.data;
 
+import androidx.annotation.NonNull;
+
+import com.example.easyride.data.model.EasyRideUser;
 import com.example.easyride.data.model.RideRequest;
+import com.example.easyride.data.model.Rider;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class RideRequestDatabase {
 
@@ -30,6 +41,18 @@ public class RideRequestDatabase {
         addHere.set(requestToBeAdded);
     }
 
-    
+    public ArrayList<RideRequest> getRequestByRiderID (String userID) {
+
+        ArrayList<RideRequest> listOfRequest = new ArrayList<>();
+        Query query = requestsRef.whereEqualTo("riderUserName", userID);
+        //requestsRef.get().where()
+
+
+        for (DocumentSnapshot document : querySnapshot.get().getDocuments()) {
+            System.out.println(document.getId());
+        }
+
+        return listOfRequest;
+    }
 
 }
