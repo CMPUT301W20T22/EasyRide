dd21443c56737941ff28a1498564c11c12c659e0
Sandip Saha Joy <sandipsa@ualberta.ca>
2020-03-12 16:28:34 -0600

    maps works

--- app/src/main/java/com/example/easyride/map/MapsActivity.java
+++ app/src/main/java/com/example/easyride/map/MapsActivity.java
@@ -3,28 +3,64 @@ package com.example.easyride.map;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.easyride.R;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+import com.google.android.material.textfield.TextInputEditText;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 
 /**
  * https://stackoverflow.com/questions/18425141/android-google-maps-api-v2-zoom-to-current-location
+ * https://github.com/ManishAndroidIos/Master-Google-Place-API
  */
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
-    private GoogleMap mMap;
-    private final int REQUEST_LOCATION_PERMISSION = 1;
+    GoogleMap mMap;
+    PlaceAutocompleteFragment place_start, place_end;
+    String place_start_string, place_end_string;
+    PlacesClient placesClient;
+    private int request_code = 1001;
+    TextInputEditText start_location_edittext;
+    TextInputEditText end_location_edittext;
+    TextInputEditText location_edittext;
+    MarkerOptions markerOptions;
+    LatLng latLng, start_location, end_location;
+    private MarkerOptions options = new MarkerOptions();
+    private ArrayList<LatLng> latlngs = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +69,25 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Places.initialize(getApplicationContext(),getString(R.string.api_key));
+
+        start_location_edittext = findViewById(R.id.start_location_EditText);
+        end_location_edittext = findViewById(R.id.end_location_EditText);
+
+        start_location_edittext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAutocompleteActivity(start_location_edittext);
+            }
+        });
+
+        end_location_edittext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAutocompleteActivity(end_location_edittext);
+            }
+        });
 
     }
 
@@ -62,5 +117,148 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(coordinate, 15);
             mMap.animateCamera(yourLocation);
         }
+
     }
+
+    private void startAutocompleteActivity(TextInputEditText x) {
+        List<com.google.android.libraries.places.api.model.Place.Field> fields = Arrays.asList(com.google.android.libraries.places.api.model.Place.Field.ID,
+                com.google.android.libraries.places.api.model.Place.Field.NAME);
+        location_edittext = x;
+        // Start the autocomplete intent.
+        Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fields).build(this);
+        startActivityForResult(intent, request_code);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == request_code) {
+            if (resultCode == RESULT_OK) {
+                Place place = Autocomplete.getPlaceFromIntent(data);
+
+                location_edittext.setText(place.getName());
+
+                if(location_edittext == start_location_edittext){
+                    start_location = place.getLatLng();
+                }
+                else if (location_edittext == end_location_edittext){
+                    end_location = place.getLatLng();
+                }
+
+
+                if(place.toString()!=null && !place.toString().equals("")){
+                    new MapsActivity.GeocoderTask().execute(place.toString());
+                }
+
+            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+                // TODO: Handle the error.
+                Status status = Autocomplete.getStatusFromIntent(data);
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
+    }
+
+//    public void place_marker(){
+//        mMap.clear();
+//        mMap.addMarker(new MarkerOptions().position(start_location).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)).title("Start here"));
+//        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(start_location,15));
+//        mMap.addMarker(new MarkerOptions().position(end_location).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)).title("End here"));
+//        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(end_location, 15));
+//    }
+
+
+
+    // An AsyncTask class for accessing the GeoCoding Web Service
+    private class GeocoderTask extends AsyncTask<String, Void, List<Address>> {
+
+        @Override
+        protected List<Address> doInBackground(String... locationName) {
+            // Creating an instance of Geocoder class
+            Geocoder geocoder = new Geocoder(MapsActivity.this);
+            List<Address> addresses = null;
+
+            try {
+                // Getting a maximum of 3 Address that matches the input text
+                addresses = geocoder.getFromLocationName(locationName[0], 3);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return addresses;
+        }
+
+        @Override
+        protected void onPostExecute(List<Address> addresses) {
+
+            if(addresses==null || addresses.size()==0){
+                Toast.makeText(MapsActivity.this, "No Location found", Toast.LENGTH_SHORT).show();
+            }
+
+            // Clears all the existing markers on the map
+            //mMap.clear();
+
+            // Adding Markers on Google Map for each matching address
+            for(int i=0;i<addresses.size();i++){
+
+                Address address = (Address) addresses.get(i);
+
+                // Creating an instance of GeoPoint, to display in Google Map
+                latLng = new LatLng(address.getLatitude(), address.getLongitude());
+
+                markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+                markerOptions.title(address.getAddressLine(0));
+                mMap.addMarker(markerOptions);
+
+                // Locate the first location
+                if(i==0)
+                    mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(latLng)      // Sets the center of the map to Mountain View
+                        .zoom(17)                   // Sets the zoom
+                        .bearing(90)                // Sets the orientation of the camera to east
+                        .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                        .build();                   // Creates a CameraPosition from the builder
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        }}
+
+
+
+//    place_start = (PlaceAutocompleteFragment)getFragmentManager().findFragmentById(R.id.place_start);
+//    place_end = (PlaceAutocompleteFragment)getFragmentManager().findFragmentById(R.id.place_end);
+//
+//        place_start.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+//        @Override
+//        public void onPlaceSelected(Place place) {
+//            place_start_string = place.getAddress().toString();
+//            mMap.clear();
+//            mMap.addMarker(new MarkerOptions().position(place.getLatLng()).icon(BitmapDescriptorFactory.defaultMarker()).title("Pickup here"));
+//            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(),15));
+//
+//        }
+//
+//        @Override
+//        public void onError(Status status) {
+//
+//        }
+//    });
+//
+//        place_end.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+//        @Override
+//        public void onPlaceSelected(Place place) {
+//            place_end_string = place.getAddress().toString();
+//            mMap.clear();
+//            mMap.addMarker(new MarkerOptions().position(place.getLatLng()).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)).title("Drop here"));
+//            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(),15));
+//
+//        }
+//
+//        @Override
+//        public void onError(Status status) {
+//
+//        }
+//    });
 }
+
+
