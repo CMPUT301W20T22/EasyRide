5c2935f23a2b5cfbb38e014cbec8e444575e6b5c
SANG LE <sqle@ualberta.ca>
2020-03-13 19:08:47 -0600

    fix the bugs and add the search bar option on DriverUI

--- app/src/main/java/com/example/easyride/MainActivity.java
+++ app/src/main/java/com/example/easyride/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.easyride;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -40,7 +41,6 @@ public class MainActivity extends AppCompatActivity {
       }
     });
 
-
-
   }
+
 }
--- app/src/main/java/com/example/easyride/ui/driver/driver_home.java
+++ app/src/main/java/com/example/easyride/ui/driver/driver_home.java
@@ -1,17 +1,19 @@
 package com.example.easyride.ui.driver;
 
-import android.app.ProgressDialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuItemCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+import androidx.appcompat.widget.SearchView;
+
 
 import com.example.easyride.R;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -67,6 +69,9 @@ public class driver_home extends AppCompatActivity {
 
     private void showData() {
 
+
+        // Get the data by geoLocation
+        // geoLocation is based on the cost of the trip, the closer the trip is the cheaper the cost
         db.collection("RideRequest").orderBy("cost", Query.Direction.ASCENDING).addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
             public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
@@ -89,4 +94,65 @@ public class driver_home extends AppCompatActivity {
             }
         });
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // inflating driver_menu
+        getMenuInflater().inflate(R.menu.driver_menu, menu);
+        // SearchView
+        MenuItem item = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // called when we pressed search button
+                searchData(query); 
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // called as and when we type
+                showData();
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void searchData(String query) {
+        // search data
+        db.collection("RideRequest").whereEqualTo("riderUserName", query.toLowerCase())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        // called when searching is successful
+                        rideRequestList.clear();
+                        for (DocumentSnapshot doc : task.getResult()) {
+                            RideRequest rideRequest = new RideRequest(doc.getString("riderUserName"),
+                                    doc.getString("pickupPoint"),
+                                    doc.getString("targetPoint"),
+                                    doc.getLong("cost").intValue());
+                            rideRequestList.add(rideRequest);
+                        }
+
+                        // adapter
+                        rideRequestListAdapter = new RideRequestListAdapter(driver_home.this,rideRequestList);
+                        // set adapter
+                        mRequestList.setAdapter(rideRequestListAdapter);
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // called when there is any error
+                        Toast.makeText(driver_home.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
 }
