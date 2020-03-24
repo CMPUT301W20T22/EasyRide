e4a030ef2b8c3032e8fad66f36e61ef1aa686bdf
Arbelaezch <arbelaezch@gmail.com>
2020-03-24 12:53:58 -0600

    small edits

--- app/src/main/java/com/example/easyride/user_profile.java
+++ app/src/main/java/com/example/easyride/user_profile.java
@@ -8,22 +8,33 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.easyride.R;
+import com.example.easyride.data.model.EasyRideUser;
+import com.example.easyride.data.model.Rider;
+import com.example.easyride.ui.login.LoginActivity;
 import com.example.easyride.ui.rider.rider_home;
+
 
 
 // User profile screen that handles Name, Contact info, and rating.
 // Should be able to edit contact info
-public class user_profile extends AppCompatActivity {
+public class user_profile extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_profile); // I DON'T KNOW WHY THIS IS RED, BUT IT WORKS SO IDK.
+        setContentView(R.layout.activity_user_profile);
 
 
         // TODO: Set up fragment to edit user's contact info.
 
-        // Button editButton = findViewById(R.id.edit_contact_button);
+
+        EasyRideUser user = getCurrentRiderInfo();
+
+        Rider settingInstance = Rider.getInstance(user);
+
+
+
+        Button editButton = findViewById(R.id.edit_contact_button);
 
 
         /*
