3d22b1b03ace3a9dc1f308ec7988cd74991269c9
Kyle Fan <kfan326@gmail.com>
2020-03-31 00:01:59 -0600

    unit tests

---
+++ app/src/test/java/com/example/easyride/RideUnitTest.java
@@ -0,0 +1,74 @@
+package com.example.easyride;
+
+import com.example.easyride.ui.rider.Ride;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RideUnitTest {
+
+    private Ride TestRide() {
+        return new Ride("origin", "destination", "1", "test_user", "10");
+    }
+
+    /**
+     * test if ride can be created
+     */
+
+    @Test
+    public void createRideTest() {
+        final Ride testRide = TestRide();
+        assertEquals(testRide.getFrom(), "origin");
+        assertEquals(testRide.getTo(), "destination");
+        assertEquals(testRide.getCost(), "1");
+        assertEquals(testRide.getUser(), "test_user");
+        assertEquals(testRide.getDistance(), "10");
+    }
+
+    /**
+     * test if ride can be modified
+     */
+    @Test
+    public void rideSetterTest() {
+        final Ride testRide = TestRide();
+
+        testRide.setFrom("start");
+        testRide.setTo("finish");
+        testRide.setCost("2");
+        testRide.setUser("");
+        testRide.setDistance("20");
+
+        assertEquals(testRide.getFrom(), "start");
+        assertEquals(testRide.getTo(), "finish");
+        assertEquals(testRide.getCost(), "2");
+        assertEquals(testRide.getUser(), "");
+        assertEquals(testRide.getDistance(), "20");
+
+        assertFalse(testRide.isRideAccepted());
+        assertFalse(testRide.isRideCompleted());
+        assertFalse(testRide.isRideConfirmAccepted());
+        assertFalse(testRide.isRidePaid());
+
+        testRide.setRidePaid(true);
+        testRide.setRideConfirmAccepted(true);
+        testRide.setRidePaid(true);
+        testRide.setRideCompleted(true);
+
+        assertTrue(testRide.isRideCompleted());
+        assertTrue((testRide.isRidePaid()));
+        assertTrue(testRide.isRideConfirmAccepted());
+        assertTrue(testRide.isRidePaid());
+
+    }
+
+
+
+
+
+
+
+
+}
